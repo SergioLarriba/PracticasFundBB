a767c69211acc342f12494696dd696220f0263a6
davidfordpickup <114607734+davidfordpickup@users.noreply.github.com>
2022-09-28 20:01:11 +0200

    Add files via upload

---
+++ Practica1/query3.sql
@@ -0,0 +1,5 @@
+with director as (select employees.employeenumber from employees where employees.reportsto IS NULL),
+report_group as (select employees.employeenumber from employees where employees.reportsto in (select * from director))
+SELECT employees.employeenumber, employees.lastname
+FROM employees
+WHERE employees.reportsto IN (select * from report_group)
