845eabb13d29bd9a0f19d1e86fe314b31541b73b
SergioLarriba <98891869+SergioLarriba@users.noreply.github.com>
2022-10-16 13:19:25 +0200

    Update query1.sql

--- Practica1/query1.sql
+++ Practica1/query1.sql
@@ -1,5 +1,10 @@
-with product_name as (select productname from products where productname='1940 Ford Pickup Truck')
-SELECT customers.customername,customers.customernumber,payments.amount 
-FROM payments join customers on customers.customernumber=payments.customernumber, products
-where products.productname in (select * from product_name)
-order by payments.amount desc
+select 
+	customers.customernumber,customers.customername,sum(payments.amount) 
+from
+	products natural join orderdetails natural join orders natural join customers natural join payments
+where 
+	products.productname = '1940 Ford Pickup Truck'
+group by 
+	customers.customernumber
+order by 
+	sum(payments.amount) desc
