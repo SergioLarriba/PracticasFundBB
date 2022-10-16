bf833773cb850ef688705da3752a85411ad8ff88
SergioLarriba <98891869+SergioLarriba@users.noreply.github.com>
2022-10-16 13:21:09 +0200

    Update query5.sql

--- Practica1/query5.sql
+++ Practica1/query5.sql
@@ -1,7 +1,13 @@
-with officenosale_group as(select ordernumber FROM orders WHERE shippeddate IS NULL and orderdate>='2003-01-01' and orderdate<='2003-12-31')
-SELECT o.country, count(o.officecode)
-FROM offices as o, orders as ord
-WHERE ord.ordernumber in (select* FROM officenosale_group)
-GRoup by o.country
-having count(o.officecode) >=1
+-- tabla con las oficinas que no han realizado ninguna venta durante el año 2003 (shippeddate is null)
+with no_sale_offices as (select o.officecode from offices o natural join employees natural join customers natural join orders ord where ord.shippeddate is NULL and orderdate>='2003-01-01' and orderdate<='2003-12-31')
+
+select 
+	o.country, count(o.officecode) as offices_with_no_sales
+from 
+	offices o
+where 
+	o.officecode in (select * from no_sale_offices)
+group by 
+	o.country
+having count(o.officecode)>=1
 order by count(o.officecode) desc 
