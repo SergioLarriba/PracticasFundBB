6b010b3d2779aa68aab6502ce262fc879530a752
SergioLarriba <98891869+SergioLarriba@users.noreply.github.com>
2022-10-16 13:20:40 +0200

    Update query4.sql

--- Practica1/query4.sql
+++ Practica1/query4.sql
@@ -1,16 +1,9 @@
--- Query4
--- hago una tabla con los productos y las unidades 
-with quant as (select p.productcode, o.quantityordered, o.ordernumber from orderdetails as o join products
-			  as p on o.productcode=p.productcode), 
-	-- hago un subconjunto que una la tabla offices con la tabla orderdetails
-	joins as (select o.officecode, orderd.productcode 
-			 from offices o join employees e on o.officecode=e.officecode --offices-employees
-			 join customers cust on e.employeenumber=cust.salesrepemployeenumber --employees-customers
-			 join orders ord on cust.customernumber=ord.customernumber --customers-orders
-			 join orderdetails orderd on ord.ordernumber=orderd.ordernumber --orders-orderdetails
-			 )
-
-select sum(quant.quantityordered),joins.officecode
-from quant join joins on quant.productcode=joins.productcode 
-group by joins.officecode
-order by sum(quant.quantityordered) desc
+select 
+	o.officecode, sum(orderdi.quantityordered)
+from 
+	orderdetails orderdi natural join orders 
+	natural join customers c 
+	join employees e natural join offices o on c.salesrepemployeenumber=e.employeenumber 
+group by 
+	o.officecode
+order by sum(orderdi.quantityordered) desc
