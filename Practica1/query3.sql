1e1f4f0d503fd36b08805f56ece1c9eef4f4fc29
SergioLarriba <98891869+SergioLarriba@users.noreply.github.com>
2022-10-16 13:20:20 +0200

    Update query3.sql

--- Practica1/query3.sql
+++ Practica1/query3.sql
@@ -1,5 +1,9 @@
 with director as (select employees.employeenumber from employees where employees.reportsto IS NULL),
-report_group as (select employees.employeenumber from employees where employees.reportsto in (select * from director))
-SELECT employees.employeenumber, employees.lastname
-FROM employees
-WHERE employees.reportsto IN (select * from report_group)
+	 report_group as (select employees.employeenumber from employees where employees.reportsto in (select * from director))
+
+select
+	employees.employeenumber, employees.lastname
+FROM 
+	employees
+WHERE 
+	employees.reportsto IN (select * from report_group)
